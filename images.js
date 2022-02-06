6b1d32b5b14882e0f7e5b847ed6351d0e53fc28d
David Weiss <davidweisspost@gmail.com>
2022-02-06 17:27:41 +0100

    Add images

---
+++ images.js
@@ -0,0 +1,6 @@
+export const images = {
+    flute: require("./assets/img/flute.png"),
+    margarita: require("./assets/img/margarita.png"),
+    highball: require("./assets/img/highball.png"),
+    "highball-long": require("./assets/img/highball-long.png"),
+};
