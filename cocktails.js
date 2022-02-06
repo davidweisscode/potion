342611b67b4ad9c63e21a97b7178ffba34207693
David Weiss <davidweisspost@gmail.com>
2022-02-06 17:27:16 +0100

    Add cocktail model file

---
+++ cocktails.js
@@ -0,0 +1,83 @@
+export const cocktails = [
+    {
+        name: "White Lady 1",
+        id: 0,
+        ingredients: {
+            Gin: "30 ml",
+            Lemon: "15 ml",
+        },
+        image: "margarita",
+    },
+    {
+        name: "Cosmopolitan",
+        id: 1,
+        ingredients: {
+            vodka: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "highball-long",
+    },
+    {
+        name: "Caipirinha",
+        id: 2,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha",
+        id: 3,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha",
+        id: 4,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha",
+        id: 5,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha",
+        id: 6,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha",
+        id: 8,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "99 ml",
+        },
+        image: "flute",
+    },
+    {
+        name: "Caipirinha End",
+        id: 9,
+        ingredients: {
+            rum: "30 ml",
+            lemon: "15 ml",
+        },
+        image: "flute",
+    },
+];
