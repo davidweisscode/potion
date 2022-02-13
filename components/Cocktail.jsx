8f5ff04b850be6b2b1a0df1892a7c76e20b6e4bd
David Weiß <davidweisspost@gmail.com>
2022-02-13 11:31:14 +0100

    Update cocktail component in list

--- components/Cocktail.jsx
+++ components/Cocktail.jsx
@@ -1,44 +1,72 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 import { images } from "../images.js";
+import { useNavigation } from '@react-navigation/native';
 
 export default function Cocktail(props) {
+    const navigation = useNavigation();
+
     return (
-        <View style={styles.item} key={props.key}>
-            <Text style={styles.name}>{props.name}</Text>
-            {Object.entries(props.ingredients).map(([key, value]) => {
-                return (
-                    <Text style={styles.ingredient} key={props.key}>{key} {value}</Text>
-                )
+        <TouchableOpacity
+            style={styles.item}
+            key={props.key}
+            onPress={() => navigation.navigate('Recipe', {
+                name: props.name, // pass a prop as param
             })}
+            activeOpacity={0.6}>
+            <View style={styles.text}>
+                <Text style={styles.name}>{props.name}</Text>
+                <Text style={styles.short}>{props.short}</Text>
+            </View>
             <Image source={images[props.image]} style={styles.image}></Image>
-        </View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginHorizontal: "4%",
+        marginVertical: "2%",
         padding: "5%",
         borderRadius: 20,
         backgroundColor: "lightgrey",
-        marginHorizontal: "4%",
-        marginVertical: "2%",
-        minWidth: "90%",
+        shadowColor: 'red',
+        shadowOpacity: 0.8,
+        shadowOffset: { width: 0, height: 0},
+        shadowRadius: 10,
+        elevation: 10,
+        borderColor: "gold",
+        borderWidth: 1,
+    },
+    text: {
+        justifyContent: "center",
+        maxWidth: "80%",
+        // backgroundColor: "#ffa",
+        borderColor: "red",
+        borderWidth: 1
     },
     name: {
-        fontSize: 24,
+        fontSize: 32,
         fontWeight: "bold",
         backgroundColor: "#bbbbbb",
     },
-    ingredient: {
+    short: {
         fontSize: 16,
+        fontStyle: "normal",
         backgroundColor: "#cccccc",
     },
     image: {
+        // position: "absolute",
+        // right: "1%",
         // maxWidth: "20%",
         // maxHeight: "20%",
-        width: 200,
-        height: 200,
+        width: 110,
+        height: 110,
         backgroundColor: "#ddd",
         tintColor: "#000",
+        borderColor: "blue",
+        borderWidth: 1,
     }
 })
